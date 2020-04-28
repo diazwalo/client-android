f62b897067b0e6826fada3b3436e2eccebf5b000
Virgil <virgil031299@gmail.com>
2020-04-28 13:01:36 +0200

    Done : Ajout du fonctionnement de la page de chargement de l'application Doing : ajout du fonctionnement de la page de login

---
+++ app/src/main/java/fr/ulille/iut/agile/client_android/LoginActivity.java
@@ -0,0 +1,22 @@
+package fr.ulille.iut.agile.client_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void onClicSubmit(View view) {
+        Intent intent=new Intent(LoginActivity.this, HomeActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
