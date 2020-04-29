bf30ddab9f6168a30173d7e3360b32f8c9538e97
Virgil <virgil031299@gmail.com>
2020-04-29 11:00:34 +0200

    Done : Resolution bug saisie dans log in TO DO : Fonctionnement du log in
    Signed-off-by: Virgil <virgil031299@gmail.com>

--- app/src/main/java/fr/ulille/iut/agile/client_android/LoginActivity.java
+++ app/src/main/java/fr/ulille/iut/agile/client_android/LoginActivity.java
@@ -2,9 +2,14 @@ package fr.ulille.iut.agile.client_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -18,5 +23,15 @@ public class LoginActivity extends AppCompatActivity {
         Intent intent=new Intent(LoginActivity.this, HomeActivity.class);
         startActivity(intent);
         finish();
+    }
+
+    protected void wrongLoginAction() {
+        Context context = getApplicationContext();
+        CharSequence text = ""+R.string.login_failed;
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+
     }
 }
