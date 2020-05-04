44f2bece34aacaf78e68d4b31d2f4ba825043940
Thibault Garcia <thibault.garcia.etu@univ-lille.fr>
2020-05-04 16:09:50 +0200

    Transformation d'un attribut en variable locale

--- app/src/main/java/fr/ulille/iut/agile/client_android/Connection.java
+++ app/src/main/java/fr/ulille/iut/agile/client_android/Connection.java
@@ -2,10 +2,9 @@ package fr.ulille.iut.agile.client_android;
 
 public class Connection {
     private final static String url = "https://groupe4.azae.eu/api/v1/";
-    public static String urlCompleted = null;
 
     public static String constructServerURL(String[] parameters) {
-        urlCompleted = Connection.url;
+        String urlCompleted = Connection.url;
 
         for (int idx = 0; idx < parameters.length; idx++) {
             urlCompleted += "/" + parameters[idx];
