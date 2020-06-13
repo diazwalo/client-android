170cd77d49291067ea716fc1c49bc7e84d7c523a
Virgil <virgil031299@gmail.com>
2020-06-13 17:52:14 +0200

    Done : visuel de l'activité permetant de voir son profil (sans le fonctionnement)
    Signed-off-by: Virgil <virgil031299@gmail.com>

--- code/app/src/main/java/com/example/unlinkedpairandroidapp/HomeActivity.java
+++ code/app/src/main/java/com/example/unlinkedpairandroidapp/HomeActivity.java
@@ -3,6 +3,7 @@ package com.example.unlinkedpairandroidapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void onClicProfil(View view) {
+        ActivitySwitcher.switchActivity(this, ProfilActivity.class, false);
     }
 }
