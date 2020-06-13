4d3b43b2a54e087867e85148121e270ee38c5b12
Virgil <virgil031299@gmail.com>
2020-06-13 19:42:32 +0200

    Done : Activity qui gere les parametres du compte de l'utilisateur (sans le fontionnement)
    Signed-off-by: Virgil <virgil031299@gmail.com>

--- code/app/src/main/java/com/example/unlinkedpairandroidapp/ProfilActivity.java
+++ code/app/src/main/java/com/example/unlinkedpairandroidapp/ProfilActivity.java
@@ -3,6 +3,7 @@ package com.example.unlinkedpairandroidapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class ProfilActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class ProfilActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profil);
+    }
+
+    public void onClicccountSettings(View view) {
+        ActivitySwitcher.switchActivity(this, AccountSettingsActivity.class, false);
     }
 }
