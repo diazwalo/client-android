c2f49c0e798055100fac108b2ddf1c0d66c05e44
Virgil <virgil031299@gmail.com>
2020-06-14 12:36:12 +0200

    Done : visuel de l'activité servant à changer le mot de passe et celle permetant de fermer son compte (sans le fonctionnement)
    Signed-off-by: Virgil <virgil031299@gmail.com>

--- code/app/src/main/java/com/example/unlinkedpairandroidapp/AccountSettingsActivity.java
+++ code/app/src/main/java/com/example/unlinkedpairandroidapp/AccountSettingsActivity.java
@@ -3,6 +3,7 @@ package com.example.unlinkedpairandroidapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class AccountSettingsActivity extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class AccountSettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_settings);
+    }
+
+    public void onClickResetPassword(View view) {
+        ActivitySwitcher.switchActivity(this, ResetPasswordActivity.class, false);
+    }
+
+    public void onClicCloseAccount(View view) {
+        ActivitySwitcher.switchActivity(this, CloseAccountActivity.class, false);
     }
 }
---
+++ code/app/src/main/java/com/example/unlinkedpairandroidapp/CloseAccountActivity.java
@@ -0,0 +1,14 @@
+package com.example.unlinkedpairandroidapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CloseAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_close_account);
+    }
+}
